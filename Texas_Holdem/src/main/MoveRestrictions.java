5f5c3ad69f48274fc1ef8edb0a15fb1f2b1c52f1
SemiRafista <rafista14@wp.pl>
2016-01-14 03:04:23 +0100

    Fix: move restrictions in two situations
    - first player's move in second auction
    - when player don't have enough money to call/bet/raise

--- Texas_Holdem/src/main/MoveRestrictions.java
+++ Texas_Holdem/src/main/MoveRestrictions.java
@@ -14,18 +14,18 @@ public final class MoveRestrictions {
 		//check player's tokens in order to specify options available
 		//in case of bet > player tokens, player can only do AllIn action
 		//if(auction.getCurrentPlayer().getPlayerTokens() < auction.getCurrentBet()){
-		//if(playerTokens < currentBet){
-		//	gui.bet.setEnabled(false);
-		//	gui.check.setEnabled(false);
-		//	gui.call.setEnabled(false);
-		//	gui.raise.setEnabled(false);
+		if(playerTokens < currentBet){
+			gui.bet.setEnabled(false);
+			gui.check.setEnabled(false);
+			gui.call.setEnabled(false);
+			gui.raise.setEnabled(false);
 		//	gui.fold.setEnabled(false);
-		//}
+		}
 		
 		//options available for first player in his first move: CHECK, BET, FOLD, ALL IN
 		//option unavailable: RAISE, CALL
 		//if(auction.movesCounter == 0){
-		if(movesCounter == 0 && PP_action == null){	
+		if(movesCounter == 0 && PP_action == ActionTaken.NONE){	
 			gui.raise.setEnabled(false);
 			gui.call.setEnabled(false);
 		}
